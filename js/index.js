bd7234ac62018c72ece0b8890527796ea170d07c
thaneeola <imranthanee@gmail.com>
2024-04-23 11:44:14 +0100

    new js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-alert("hello Our site is having issues with javascript")
-// let toggle = document.getElementById("toggle");
-// let link = document.getElementById("link");
-// let toggleEffect = (link.style.display = "none");
-// toggle.addEventListener("onclick", (link.style.display = "flex"));
-// // if ((toggleEffect)) {
-//   link.style.display = "flex";
-// } else {
-//   link.style.display = "none";
-// }
+alert("hello Our site is having issues with javascript");
+let dive = document.getElementById("toggle");
+let link = document.getElementById("link");
+console.log(dive, link);
+dive.addEventListener("click", show);
+function show() {
+  if (link.style.display == "flex") {
+    link.style.display = "none";
+  } else {
+    link.style.display = "flex";
+  }
+}
