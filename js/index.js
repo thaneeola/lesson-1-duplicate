c163cb84ac10d44d2f0fd133a99efbc9467b2ba0
thaneeola <imranthanee@gmail.com>
2024-04-23 01:52:24 +0100

    new

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// alert("hello")
+let toggle = document.getElementById("toggle");
+let link = document.getElementById("link")
+let toggleEffect = link.style.display ="none"
+function (toggleEffect) {
+    if (condition) {
+        
+    } else {
+        
+    }
+})
